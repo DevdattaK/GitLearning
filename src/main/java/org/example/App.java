d4c4af2b7ff2183e37cffae29f5f8977e9688c16
Devdatta <devdattakwd@yahoo.com>
2020-04-04 16:50:58 -0400

    test commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,18 @@
 package org.example;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Map<String, Double> testMap = new HashMap<>();
+
+        long count = testMap.values().stream().count();
+
+        System.out.println(count);
     }
 }
